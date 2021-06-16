be82b0e2782a4906805c6d0104f1f7dcd153d855
Jasmine Christine <85191466+JasChristine@users.noreply.github.com>
2021-06-15 22:06:16 -0400

    dogyears

---
+++ DogYears.js
@@ -0,0 +1,15 @@
+//My age
+var myAge = 25;
+//Early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+//Later Years
+let laterYears = myAge -2;
+//Number of dog years
+laterYears *= 4;
+//Me in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+
+var myName = "Jasmine".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`)
