28d9333cad5e04bc6d1d709c32d5b260805b9f29
Jasmine Christine <85191466+JasChristine@users.noreply.github.com>
2021-06-20 23:57:07 -0400

    Create WhaleTalk.js

---
+++ WhaleTalk.js
@@ -0,0 +1,18 @@
+const input = "what does the fox say";
+const vowels = ["a", "e", "i", "o", "u"];
+let resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  //console.log(input[inputIndex]);
+  for (let vowel = 0; vowel < vowels.length; vowel++) {
+    if (input[inputIndex] === vowels[vowel]) {
+      if (input[inputIndex] === "e") {
+        resultArray.push("ee");
+      } else {
+        resultArray.push(input[inputIndex]);
+      }
+    }
+  }
+}
+
+console.log(resultArray.join("").toUpperCase());
