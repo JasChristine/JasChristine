37b6794ca698786163a689a91d89c9f2acc32357
Jasmine Christine <85191466+JasChristine@users.noreply.github.com>
2021-07-06 18:55:26 -0400

    .

--- codingBootcamp/javaScript/challengesAndProjects/src/moveBob.js
+++ codingBootcamp/javaScript/challengesAndProjects/src/moveBob.js
@@ -3,8 +3,8 @@ const bob = document.querySelector(".face");
 let count = 0;
 
 const moveBob = () => {
-    count += 50
-    bob.style.left = count + "px";
+  count += 50;
+  bob.style.left = count + "px";
 };
 
 bob.addEventListener("click", moveBob);
