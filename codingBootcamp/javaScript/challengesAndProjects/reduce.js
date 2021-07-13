023aaeb5e747c370affe43441f3efcd3f772051f
Jasmine Christine <85191466+JasChristine@users.noreply.github.com>
2021-07-13 19:19:28 -0400

    reduce

---
+++ codingBootcamp/javaScript/challengesAndProjects/reduce.js
@@ -0,0 +1,8 @@
+//.reduce()
+//var.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+const scores = [34, , 56, 24, 31];
+
+const totalScores = scores.reduce((totalScore, score) => totalScore + score);
+
+console.log(totalScores);
